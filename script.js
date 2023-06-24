ab2d7f8d84bb0acdb0edfb55c77765dc2b108930
Charlie-Pencal <cpencal@gmail.com>
2023-06-24 17:50:18 -0300

    estruturas completas

--- script.js
+++ script.js
@@ -46,21 +46,37 @@ converter.addEventListener("click", function (conv) {
   } else if (medidaOrigem.value == "metros" && medidaSaida.value == "polegadas") {
     resultado.innerHTML = metroPolegada(valor);
   } else if (medidaOrigem.value == "centimetros" && medidaSaida.value == "metros") {
+    resultado.innerHTML = centimentroMetro(valor);
   } else if (medidaOrigem.value == "centimetros" && medidaSaida.value == "polegadas") {
+    resultado.innerHTML = centimetroPolegada(valor);
   } else if (medidaOrigem.value == "polegadas" && medidaSaida.value == "metros") {
+    resultado.innerHTML = polegadaMetro(valor);
   } else if (medidaOrigem.value == "polegadas" && medidaSaida.value == "centimetros") {
+    resultado.innerHTML = polegadaCentimetro(valor);
   } else if (medidaOrigem.value == "quilogramas" && medidaSaida.value == "gramas") {
+    resultado.innerHTML = quilogramaGrama(valor);
   } else if (medidaOrigem.value == "quilogramas" && medidaSaida.value == "libras") {
+    resultado.innerHTML = quilogramaLibra(valor);
   } else if (medidaOrigem.value == "gramas" && medidaSaida.value == "quilogramas") {
+    resultado.innerHTML = gramaQuilograma(valor);
   } else if (medidaOrigem.value == "gramas" && medidaSaida.value == "libras") {
+    resultado.innerHTML = gramaLibra(valor);
   } else if (medidaOrigem.value == "libras" && medidaSaida.value == "quilogramas") {
+    resultado.innerHTML = libraQuilograma(valor);
   } else if (medidaOrigem.value == "libras" && medidaSaida.value == "gramas") {
+    resultado.innerHTML = libraGrama(valor);
   } else if (medidaOrigem.value == "celsius" && medidaSaida.value == "fahrenheit") {
+    resultado.innerHTML = celsiusFahrenheit(valor)
   } else if (medidaOrigem.value == "celsius" && medidaSaida.value == "kelvin") {
+    resultado.innerHTML = celsiusKelvin(valor)
   } else if (medidaOrigem.value == "fahrenheit" && medidaSaida.value == "celsius") {
+    resultado.innerHTML = fahrenheitCelsius(valor)
   } else if (medidaOrigem.value == "fahrenheit" && medidaSaida.value == "kelvin") {
+    resultado.innerHTML = fahrenheitKelvin(valor)
   } else if (medidaOrigem.value == "kelvin" && medidaSaida.value == "celsius") {
+    resultado.innerHTML = kelvinCelsius(valor)
   } else if (medidaOrigem.value == "kelvin" && medidaSaida.value == "fahrenheit") {
+    resultado.innerHTML = kelvinFahrenheit(valor)
   } else if (medidaOrigem.value == medidaSaida.value) {
     alert("Escolha unidades de medida diferentes");
   }
@@ -78,18 +94,63 @@ function centimentroMetro(valor) {
   var centimetroParaMetro = valor.value / 100;
   return centimetroParaMetro;
 }
-function centimetroPolegada() {}
-function polegadaMetro() {}
-function polegadaCentimetro() {}
-function quilogramaGrama() {}
-function quilogramaLibra() {}
-function gramaQuilograma() {}
-function gramaLibra() {}
-function libraQuilograma() {}
-function libraGrama() {}
-function celsiusFahrenheit() {}
-function celsiusKelvin() {}
-function fahrenheitCelsius() {}
-function fahrenheitKelvin() {}
-function kelvinCelsius() {}
-function kelvinFahrenheit() {}
+function centimetroPolegada(valor) {
+  var centimetroParaPolegada = valor.value / 2.54;
+  return centimetroParaPolegada;
+}
+function polegadaMetro(valor) {
+  var polegadaParaMetro = valor.value / 39.37;
+  return polegadaParaMetro;
+}
+function polegadaCentimetro(valor) {
+  var polegadaParaCentimetro = valor.value * 2.54;
+  return polegadaParaCentimetro;
+}
+function quilogramaGrama(valor) {
+  var quilogramaParaGrama = valor.value * 1000;
+  return quilogramaParaGrama;
+}
+function quilogramaLibra(valor) {
+  var quilogramaParaLibra = valor.value * 2.205;
+  return quilogramaParaLibra;
+}
+function gramaQuilograma(valor) {
+  var gramaParaQuilograma = valor.value / 1000;
+  return gramaParaQuilograma;
+}
+function gramaLibra(valor) {
+  var gramaParaLibra = valor.value / 453.6;
+  return gramaParaLibra;
+}
+function libraQuilograma(valor) {
+  var libraParaQuilograma = valor.value / 2.205;
+  return libraParaQuilograma;
+}
+function libraGrama(valor) {
+  var libraParaGrama = valor.value * 453.6;
+  return libraParaGrama;
+}
+function celsiusFahrenheit(valor) {
+  var celsiusParaFahrenheit = (valor.value * 9 / 5) + 32;
+  return celsiusParaFahrenheit
+}
+function celsiusKelvin(valor) {
+  var celsiusParaKelvin = valor.value + 273.15
+  return celsiusParaKelvin
+}
+function fahrenheitCelsius(valor) {
+  var fahrenheitParaCelsius = (valor.value - 32) * 5 / 9
+  return fahrenheitParaCelsius
+}
+function fahrenheitKelvin(valor) {
+  var fahrenheitParaKelvin = (valor.value - 32) * 5 / 9 + 273.15
+  return fahrenheitParaKelvin
+}
+function kelvinCelsius(valor) {
+  var kelvinParaCelsius = valor.value - 273.15
+  return kelvinParaCelsius
+}
+function kelvinFahrenheit(valor) {
+  var kelvinParaFahrenheit = (valor.value - 273.15) * 9 / 5 + 32
+  return kelvinParaFahrenheit
+}
