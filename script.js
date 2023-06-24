10ebc823c9eb224c676f43b8b7c463216cb88ea8
Charlie-Pencal <cpencal@gmail.com>
2023-06-24 16:40:12 -0300

    html, css e js corrigidos e inicio das estruturas

--- script.js
+++ script.js
@@ -3,18 +3,48 @@
 // var temperatura = ["Celsius", "Fahrenheit", "Kelvin"];
 
 var categoriaSelecionada = document.getElementById("categoria");
-var medidaOrigem = document.getElementById("medidaOrigem");
-var medidaSaida = document.getElementById("medidaSaida");
+var valor = document.getElementById("valor");
 
 categoriaSelecionada.addEventListener("change", function () {
   if (categoriaSelecionada.value == "comprimento") {
+    medidaOrigem.innerHTML = `  
+    <option value="metros">Metros</option>
+    <option value="centimetros">Centímetros</option>
+    <option value="polegadas">Polegadas</option>`;
+    medidaSaida.innerHTML = `
+    <option value="metros">Metros</option>
+    <option value="centimetros">Centímetros</option>
+    <option value="polegadas">Polegadas</option>`;
   } else if (categoriaSelecionada.value == "peso") {
+    medidaOrigem.innerHTML = `  
+    <option value="quilogramas">Quilogramas</option>
+    <option value="gramas">Gramas</option>
+    <option value="libras">Libras</option>`;
+    medidaSaida.innerHTML = ` 
+    <option value="quilogramas">Quilogramas</option>
+    <option value="gramas">Gramas</option>
+    <option value="libras">Libras</option>`;
   } else if (categoriaSelecionada.value == "temperatura") {
+    medidaOrigem.innerHTML = `  
+    <option value="celsius">Celsius</option>
+    <option value="fahrenheit">Fahrenheit</option>
+    <option value="kelvin">Kelvin</option>`;
+    medidaSaida.innerHTML = `  
+    <option value="celsius">Celsius</option>
+    <option value="fahrenheit">Fahrenheit</option>
+    <option value="kelvin">Kelvin</option>`;
   }
 });
-converter.addEventListener("click", function () {
+
+var medidaOrigem = document.getElementById("medidaOrigem");
+var medidaSaida = document.getElementById("medidaSaida");
+
+converter.addEventListener("click", function (conv) {
+  conv.preventDefault();
   if (medidaOrigem.value == "metros" && medidaSaida.value == "centimetros") {
+    resultado.innerHTML = metroCentimetro(valor);
   } else if (medidaOrigem.value == "metros" && medidaSaida.value == "polegadas") {
+    resultado.innerHTML = metroPolegada(valor);
   } else if (medidaOrigem.value == "centimetros" && medidaSaida.value == "metros") {
   } else if (medidaOrigem.value == "centimetros" && medidaSaida.value == "polegadas") {
   } else if (medidaOrigem.value == "polegadas" && medidaSaida.value == "metros") {
@@ -31,12 +61,23 @@ converter.addEventListener("click", function () {
   } else if (medidaOrigem.value == "fahrenheit" && medidaSaida.value == "kelvin") {
   } else if (medidaOrigem.value == "kelvin" && medidaSaida.value == "celsius") {
   } else if (medidaOrigem.value == "kelvin" && medidaSaida.value == "fahrenheit") {
+  } else if (medidaOrigem.value == medidaSaida.value) {
+    alert("Escolha unidades de medida diferentes");
   }
 });
 
-function metroCentimentro() {}
-function metroPolegada() {}
-function centimentroMetro() {}
+function metroCentimetro(valor) {
+  var metroParaCentimetro = valor.value * 100;
+  return metroParaCentimetro;
+}
+function metroPolegada(valor) {
+  var metroParaPolegada = valor.value * 39.37;
+  return metroParaPolegada;
+}
+function centimentroMetro(valor) {
+  var centimetroParaMetro = valor.value / 100;
+  return centimetroParaMetro;
+}
 function centimetroPolegada() {}
 function polegadaMetro() {}
 function polegadaCentimetro() {}
